b25b1e173bdd9c74a3e3c3eefd169527874f1a6a
Torkil Vatne <torkil95@gmail.com>
2022-04-18 18:01:51 +0200

    Open modal with field info

--- app/src/components/Dashboard.js
+++ app/src/components/Dashboard.js
@@ -6,13 +6,28 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
+import Box from "@mui/material/Box";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Dashboard = (props) => {
   const [rows, setRows] = useState([]);
   const [selectedRow, setSelectedRow] = useState(null);
+  const handleClose = () => setSelectedRow(null);
 
   useEffect(() => {
     // GET request using fetch inside useEffect React hook
@@ -22,6 +37,8 @@ const Dashboard = (props) => {
 
     // empty dependency array means this effect will only run once (like componentDidMount in classes)
   }, []);
+
+  console.log(selectedRow);
 
   return (
     <div className="App">
@@ -50,7 +67,7 @@ const Dashboard = (props) => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows.map((row) => (
+                {rows.map((row, index) => (
                   <TableRow
                     key={row.id}
                     sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -61,13 +78,30 @@ const Dashboard = (props) => {
                     <TableCell align="right">{row["en_us"]}</TableCell>
                     <TableCell align="right">{row["no_nb"]}</TableCell>
                     <TableCell align="right">
-                      <Button color="primary">Edit</Button>
+                      <Button
+                        color="primary"
+                        onClick={() => setSelectedRow(index)}
+                      >
+                        Edit
+                      </Button>
                     </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
           </TableContainer>
+          {selectedRow !== null && (
+            <Modal
+              open={selectedRow !== null}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <p>Edit field '{rows[selectedRow]["id"]}'</p>
+              </Box>
+            </Modal>
+          )}
         </div>
       </header>
     </div>
